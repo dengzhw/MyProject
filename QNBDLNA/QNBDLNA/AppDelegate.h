d5bea81f4d9744654a7b9585f7c022f4db225d9a
milodeng <milodeng@tencent.com>
2017-03-22 10:35:44 +0800

    create project

---
+++ QNBDLNA/QNBDLNA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QNBDLNA
+//
+//  Created by limodeng on 2017/3/22.
+//  Copyright © 2017年 limodeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QNBDLNA/QNBDLNA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QNBDLNA
+//
+//  Created by limodeng on 2017/3/22.
+//  Copyright © 2017年 limodeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
